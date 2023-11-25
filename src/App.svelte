040d71bfd4a1bda7098b03d1709dc2aed874de63
Lee-TaeGeon <114649350+Lee-TaeGeon@users.noreply.github.com>
2023-11-26 00:50:18 +0900

    유저 인증까지 파이어베이스로 구현

--- src/App.svelte
+++ src/App.svelte
@@ -5,13 +5,46 @@
   import Write from "./pages/Write.svelte";
   import Router from "svelte-spa-router";
   import "./css/style.css";
+  import { user$ } from "./store";
+  import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { onMount } from "svelte";
+  import Loading from "./pages/Loading.svelte";
+  import MyPage from "./pages/MyPage.svelte";
+
+  let isLoading = true;
+
+  const auth = getAuth();
+
+  const checkLogin = async () => {
+    console;
+    const token = localStorage.getItem("token");
+    if (!token) return (isLoading = false);
+
+    const credential = GoogleAuthProvider.credential(null, token);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
 
   const routes = {
     "/": Main,
-    "/login": Login,
     "/signup": Signup,
     "/write": Write,
+    "/my": MyPage,
   };
+
+  onMount(() => checkLogin());
 </script>
 
-<Router {routes} />
+{#if isLoading}
+  <Loading />
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
